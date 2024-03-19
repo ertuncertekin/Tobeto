efe1982e6cee013168650a66909fbff692544b08
Ertunç Ertekin <149241136+ertuncertekin@users.noreply.github.com>
2024-03-19 16:48:59 +0300

    Delete Ödevler/5.Ödev/Cosmetics.java

--- Ödevler/5.Ödev/Cosmetics.java
+++
@@ -1,8 +0,0 @@
-package eCommerce;
-
-public class Cosmetics implements Categories{
-    @Override
-    public void categoryName(String name) {
-        System.out.println("Cosmetic: " + name);
-    }
-}

407bb5ae7912a7eb33e0fc8fd1fa7083c31bf755
Ertunç Ertekin <149241136+ertuncertekin@users.noreply.github.com>
2024-04-07 17:14:59 +0300

    Delete ecommercePair2 directory

--- ecommercePair2/src/main/java/com/tobeto/ecommercePair2/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tobeto.ecommercePair2.repositories;
-
-import com.tobeto.ecommercePair2.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Integer> {
-
-}
--- ecommercePair2/src/main/java/com/tobeto/ecommercePair2/services/abstracts/UserService.java
+++
@@ -1,5 +0,0 @@
-package com.tobeto.ecommercePair2.services.abstracts;
-
-public interface UserService {
-    void add();
-}

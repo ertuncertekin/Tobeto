56bf1de6ea2e597e0aad26f58ea8e3a0f8de58eb
Ertunç Ertekin <149241136+ertuncertekin@users.noreply.github.com>
2024-05-30 15:25:35 +0300

    Add files via upload

---
+++ Ödevler/1. Ödev/V08_merhabadunya_degiskenler_veritipleri.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println("Merhaba Java");
+        System.out.println("//////////////////////////////");
+        int number=12;
+        String person="ogrenci sayısı";
+        System.out.println(person+" " + number);
+        System.out.println("//////////////////////////////");
+        char karakter='a';
+        double number2=12.5;
+        boolean dogruMu=false;
+
+    }
+}
